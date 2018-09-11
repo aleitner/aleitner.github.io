6483550903ea2fa7184b402f59094859c30d14a2
Alexander Leitner <leitnersalex@gmail.com>
2018-09-11 12:29:56 -0400

    Add pgen to projects

--- assets/javascript/pages/projects.js
+++ assets/javascript/pages/projects.js
@@ -14,5 +14,6 @@ this.projects = {
             <a href="https://github.com/aleitner/FilePiece">FilePiece</a> - Library for concurrently writing/reading files in golang<br />
             <a href="https://github.com/aleitner/shameful_gavin">Shameful Gavin</a> - Chrome plugin for shaming my friend<br />
             <a href="https://github.com/aleitner/libmapstore">libmapstore</a> - Pseudo filesystem for storing data in files<br />
+            <a href="/pgen">Password Generator</a> - <a href="https://github.com/aleitner/pgen">Code</a> | <a href="https://github.com/aleitner/aleitner.github.io/blob/master/pgen.html">Website</a><br />
           `
 }

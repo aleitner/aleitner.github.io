12564d9ba22a7e769bac9891f348095ce385a3b0
Alexander Leitner <leitnersalex@gmail.com>
2019-04-23 09:53:22 -0400

    Update projects.js

--- assets/javascript/pages/projects.js
+++ assets/javascript/pages/projects.js
@@ -11,11 +11,14 @@ this.projects = {
             <a href="https://github.com/storj/libstorj">Storj Client Library</a><br />
             <a href="https://github.com/storj/storj">Storj Go Library</a><br />
             <br />
+            <u>Games</u><br /><br />  
+            <a href="https://spacigrade.github.io/">Made in 1 day at a Game Jam. Survive as a tardigrade in space!</a><br />
+            <br />
             <u>Other</u><br /><br />
             <a href="https://github.com/aleitner/FilePiece">FilePiece</a> - Library for concurrently writing/reading files in golang<br />
             <a href="https://github.com/aleitner/shameful_gavin">Shameful Gavin</a> - Chrome plugin for shaming my friend<br />
             <a href="https://github.com/aleitner/libmapstore">libmapstore</a> - Pseudo filesystem for storing data in files<br />
             <a href="/pgen">Password Generator</a> - Generate unique deterministic passwords. Converted to a website using <a href="https://github.com/gopherjs/gopherjs">gopherjs</a>. <a href="https://github.com/aleitner/pgen">github</a><br />
             <a href="https://gallery.chuteki.com">Art gallery</a> - Website built for <a href="https://twitter.com/_chuteki">chuteki</a> to display their art.<br />
-          `
+            `
 }
